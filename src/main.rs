a8b108e7589ee76a656364cd0928be11e6228143
Oriol Burgaya <oriolbv@gmail.com>
2020-06-16 07:18:05 +0200

    GUI architecture almost defined

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,45 @@ use orbtk::prelude::*;
 
 use orbtk::{Application, Window};
 
+#[derive(Copy, Clone)]
+enum ProgressEvent {
+    Advance(f64),
+    Reset,
+    SetToFull,
+}
+
+#[derive(Default, AsAny)]
+struct MainViewState {
+    action: Option<ProgressEvent>,
+}
+
+widget!(MainView<MainViewState>);
+
+impl MainViewState {
+    fn action(&mut self, action: impl Into<Option<ProgressEvent>>) {
+        self.action = action.into();
+    }
+}
+
+impl State for MainViewState {
+    fn update(&mut self, _: &mut Registry, ctx: &mut Context) {
+        if let Some(action) = self.action {
+           
+            self.action = None;
+        }
+    }
+}
+
+impl Template for MainView {
+    fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
+        self.child(
+            Stack::new()
+                .build(ctx),
+        )
+    }
+}
+
+
 fn main() {
     Application::new()
     .window(|ctx| {
@@ -9,6 +48,7 @@ fn main() {
             .title("Main window")
             .position((100.0, 100.0))
             .size(420.0, 420.0)
+            .child(MainView::new().build(ctx))
             .build(ctx)
     })
     .run();
@@ -29,3 +69,4 @@ fn main() {
 	let vsync : i32 = 1;
     
 }
+
