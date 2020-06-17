c511e93f49d7ca6941563922483b480c514a4d12
Oriol Burgaya <oriolbv@gmail.com>
2020-06-17 07:57:21 +0200

    GUI migrated to gtk-rs

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,67 @@
-use orbtk::prelude::*;
-
-use orbtk::{Application, Window};
-
 mod main_view;
 
-#[derive(Copy, Clone)]
-enum ProgressEvent {
-    Advance(f64),
-    Reset,
-    SetToFull,
+extern crate gio;
+extern crate gtk;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use gtk::{ApplicationWindow, Builder, Button, MessageDialog};
+
+use std::env::args;
+
+fn build_ui(application: &gtk::Application) {
+    let glade_src = include_str!("builder_basics.glade");
+    let builder = Builder::new_from_string(glade_src);
+
+    let window: ApplicationWindow = builder.get_object("window1").expect("Couldn't get window1");
+    window.set_application(Some(application));
+    let bigbutton: Button = builder.get_object("button1").expect("Couldn't get button1");
+    let dialog: MessageDialog = builder
+        .get_object("messagedialog1")
+        .expect("Couldn't get messagedialog1");
+
+    bigbutton.connect_clicked(move |_| {
+        dialog.run();
+        dialog.hide();
+    });
+
+    window.show_all();
 }
-
-#[derive(Default, AsAny)]
-struct MainViewState {
-    action: Option<ProgressEvent>,
-}
-
-widget!(MainView<MainViewState>);
-
-impl MainViewState {
-    fn action(&mut self, action: impl Into<Option<ProgressEvent>>) {
-        self.action = action.into();
-    }
-}
-
-impl State for MainViewState {
-    fn update(&mut self, _: &mut Registry, ctx: &mut Context) {
-        if let Some(action) = self.action {
-           
-            self.action = None;
-        }
-    }
-}
-
-impl Template for MainView {
-    fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
-        self.child(
-            Stack::new()
-                .build(ctx),
-        )
-    }
-}
-
 
 fn main() {
-    Application::new()
-    .window(|ctx| {
-        Window::new()
-            .title("Main window")
-            .position((100.0, 100.0))
-            .size(420.0, 420.0)
-            .child(MainView::new().build(ctx))
-            .build(ctx)
-    })
-    .run();
+    
+    println!("HOLI");
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.builder_basics"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
 
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+
+    // main_view::init_main_view();
 
     // Init variables
-    let wsc : i32 = 1;
-	let screen_size : i32 = 2;
-	let flipd : i32 = 0;
-	let wsShades : i32 = 0;
-	let wsCycles : i32 = 0;
-	let wsLine : i32 = 0;
-	let rom_size : i32 = 128000000;
-	let wsMakeScr : i32 = 0;
-	let fr : i32 = 0;
-	let frameskip : i32 = 1;
-	let wsVMode : i32 = -1;
-    let vsync : i32 = 1;
+    // let wsc : i32 = 1;
+	// let screen_size : i32 = 2;
+	// let flipd : i32 = 0;
+	// let wsShades : i32 = 0;
+	// let wsCycles : i32 = 0;
+	// let wsLine : i32 = 0;
+	// let rom_size : i32 = 128000000;
+	// let wsMakeScr : i32 = 0;
+	// let fr : i32 = 0;
+	// let frameskip : i32 = 1;
+	// let wsVMode : i32 = -1;
+    // let vsync : i32 = 1;
     
     // Main Loop
-    while true {
-        println!("Main Loop");
-    }
+    // loop { 
+    //     println!("Main Loop");
+    // }
 }
--- src/main_view.rs
+++ src/main_view.rs
@@ -1,39 +1,4 @@
 
-
-#[derive(Copy, Clone)]
-enum ProgressEvent {
-    Advance(f64),
-    Reset,
-    SetToFull,
-}
-
-#[derive(Default, AsAny)]
-struct MainViewState {
-    action: Option<ProgressEvent>,
-}
-
-widget!(MainView<MainViewState>);
-
-impl MainViewState {
-    fn action(&mut self, action: impl Into<Option<ProgressEvent>>) {
-        self.action = action.into();
-    }
-}
-
-impl State for MainViewState {
-    fn update(&mut self, _: &mut Registry, ctx: &mut Context) {
-        if let Some(action) = self.action {
-           
-            self.action = None;
-        }
-    }
-}
-
-impl Template for MainView {
-    fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
-        self.child(
-            Stack::new()
-                .build(ctx),
-        )
-    }
+pub fn init_main_view() {
+    println!("gas2")
 }
